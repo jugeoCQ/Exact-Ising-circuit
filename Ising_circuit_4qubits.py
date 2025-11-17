7d625dd1b7f146dcef3a38b85e3d928d7ee46210
Juliette Geoffrion <juliette.geoffrion@calculquebec.ca>
2025-11-17 07:44:43 -0500

    Refactor 4 qubit circuit

--- Ising_circuit_4qubits.py
+++ Ising_circuit_4qubits.py
@@ -6,50 +6,36 @@ def theta_k(h, n, k): #the only place where h comes in is theta which determines
                        np.sqrt((h - np.cos(2 * np.pi * k / n))**2 + np.sin(2 * np.pi * k / n)**2))
 
 
-def B(wires, theta_k, n, h):
-    q0 = wires[0]
-    q1 = wires[1]
-    
+def B_dag(q0, q1, theta_k, n, h): # 
     qml.PauliX(wires = q1)
     qml.CNOT(wires = [q1, q0])
-    qml.ctrl(qml.RX, control = q0)(theta_k, wires = q1)
+    qml.ctrl(qml.RX, control = q0)(-theta_k, wires = q1) #Use -theta_k in the dag operation
     qml.CNOT(wires = [q1, q0])
     qml.PauliX(wires = q1)   
 
-#Ok for some reason this works but I think
-#FS = qml.FermionicSWAP(phi = np.pi, wires = [0,1]).matrix() #SWAP + CZ
-#SE = qml.SingleExcitation(phi = np.pi/2, wires = [0, 1]).matrix()
-
-#F2 = np.real(FS @ SE)
-def Fdag(wires, k):
-    q0 = wires[0]
-    q1 = wires[1]
-    
-    #these two give F2 which is hermitian so F_2^dag = F_2
-    #FIGURE OUT HOW THIS RELATES TO QFT BETTER
-    qml.FermionicSWAP(phi = np.pi, wires = wires) 
-    qml.SingleExcitation(phi = np.pi/2, wires = wires)
-
-    #Twiddle factor for the odd sector (for k = 0, its just identity so ignore)
-    #Take the dag because this one is not hermitian
-    if k == 1: #for n = 4 specifically. Not general to all n       
-        qml.adjoint(qml.S(wires = q0)) #pi/2
+def F_kn_dag(q0, q1, k, n): #dagged so taking - 2pi k/n in the phase shift
+    if k!= 0:
+        qml.PhaseShift(-2*np.pi*k/n, wires = q0) 
+    qml.CNOT(wires = [q0, q1])
+    qml.ctrl(qml.Hadamard, control = q1)(q0)
+    qml.CNOT(wires = [q0, q1])
+    qml.CZ(wires = [q0, q1])
 
 def Udis(h):
     theta = theta_k(h = h, n = 4, k = 1)
-    B(wires = [0,1], theta_k = theta, n = 4, h = h) #dag?
+    B_dag(0,1, theta_k = theta, n = 4, h = h) 
     #B at k = 0 is identity at h > 1 so no need to include it 
     #FIGURE OUT WHATS UP AT h < 1 ?
 
     #Recursive QFT structure
     #This is essentially just standard QFT but the SWAP part has to be fermionic because of the JW transform
-    Fdag(wires = [0,1], k = 1) #dag
-    Fdag(wires = [2,3], k = 0) #dag #just F_2 no twiddle
+    F_kn_dag(0,1, k = 1, n = 4) 
+    F_kn_dag(2,3, k = 0, n = 4) 
 
     qml.FermionicSWAP(phi = np.pi, wires = [1,2])
 
-    Fdag(wires = [0,1], k = 0) #dag #just F_2 no twiddle
-    Fdag(wires = [2,3], k = 0) #dag #just F_2 no twiddle
+    F_kn_dag(0,1, k = 0, n = 4) 
+    F_kn_dag(2,3, k = 0, n = 4) 
     
     qml.FermionicSWAP(phi = np.pi, wires = [1,2])
 
@@ -58,7 +44,6 @@ def Ising(h):
     if h < 1: #otherwise prepare the state in |0001> #odd sector. #PBC after JW?
         qml.PauliX(wires = 3)
 
-    # Notation is sketchy but this is the reverse direction
+    # Notation is confusing but this is the dagged direction (take computational basis states to Ising eigenstates)
     #So we're doing bogoliubov then fourier
-    #cant remember if that's U_dis or U^dag_dis but it doesn't matter just notation
     Udis(h)
